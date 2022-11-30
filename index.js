8f02439ece54ceccfd063b390c7f73d2926a8bd2
Samer taha <alrazy1@gmail.com>
2022-11-30 16:47:53 +0200

    Remove the node_modules

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import * as dotenv from 'dotenv'
+dotenv.config()
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
 
-import postRoutes from './routes/posts.js';
+import postRoutes from './routes/posts.js'
 
-const app = express();
+const app = express()
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
-app.use(cors());
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors())
 
-app.use('/posts', postRoutes);
-const CONNECTION_URL =
-  'mongodb+srv://samertaha:5NqAeoPR@cluster0.ccilm2c.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
+app.use('/posts', postRoutes)
+const CONNECTION_URL = process.env.MONGODB_URL
+const PORT = process.env.PORT || 5000
 
 mongoose
   .connect(CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() =>
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
-  )
-  .catch((err) => console.log(err.message));
+  .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+  .catch((err) => console.log(err.message))
