02680b12137b0d08424ea4fff97473989c1c54e3
Samer taha <alrazy1@gmail.com>
2023-02-12 13:40:21 +0200

    latest commit

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 
 import postRoutes from './routes/posts.js'
+import userRoutes from './routes/users.js'
 
 const app = express()
 
@@ -14,6 +15,8 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
 
 app.use('/posts', postRoutes)
+app.use('/users', userRoutes)
+
 const CONNECTION_URL = process.env.MONGODB_URL
 const PORT = process.env.PORT || 5000
 
